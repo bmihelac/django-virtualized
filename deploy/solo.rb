f8fca042ec808757bc4271553cbc291e779df6a3
Bojan Mihelac <bmihelac@mihelac.org>
2012-05-29 13:05:54 +0200

    Working solo.rb

--- deploy/solo.rb
+++ deploy/solo.rb
@@ -1,9 +1,3 @@
 #log_level :info
-#cookbook_path "/etc/chef/cookbooks"
-#json_attribs "/etc/chef/node.json"
-#role_path "/etc/chef/roles"
-root = File.dirname(__FILE__)
-
-file_cache_path root
-cookbook_path root + '/cookbooks'
-
+cookbook_path "/etc/chef/cookbooks"
+json_attribs "/etc/chef/node.json"
